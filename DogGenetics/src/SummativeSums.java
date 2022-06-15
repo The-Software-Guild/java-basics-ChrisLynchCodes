0a1eb81fdde19ba57f71fb81b33bef49af14e6b1
Chris Lynch - Laptop <chrislynchcodes@gmail.com>
2022-06-15 18:12:10 +0100

    final

--- DogGenetics/src/SummativeSums.java
+++ DogGenetics/src/SummativeSums.java
@@ -1,2 +1,11 @@
 public class SummativeSums {
+
+    public static int summativeSums(int[] nums) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum;
+
+    }
 }
